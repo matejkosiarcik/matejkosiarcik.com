7b907f430023746238c46768560f75e325f379ba
Matej Košiarčik <matejkosiarcik@gmail.com>
2017-08-05 13:36:36 +0200

    [ADD] Make build-mustache work on individual files

--- utils/internal/build-mustache.py
+++ utils/internal/build-mustache.py
@@ -8,41 +8,61 @@ from __future__ import absolute_import, division, print_function, unicode_litera
 import argparse
 import json
 import os
-from functional import seq
 import pystache
 import six
 
 # parse arguments
 parser = argparse.ArgumentParser()
-parser.add_argument("-o", "--output")
+parser.add_argument("-o", "--output", help="Output path (file or directory)")
+parser.add_argument("-d", "--data", help="Directory path for json-data and partial-html")
 arguments = parser.parse_args()
 
 # validate arguments
 if arguments.output is None:
-    print("Output is missing")
+    print("Missing output filepath")
+    print(arguments.usage)
     exit(1)
-output = arguments.output
+if arguments.data is None:
+    print("Missing data filepath")
+    print(arguments.usage)
+    exit(1)
 
-# get markup template
-markup_path = os.path.join("src", "shared", "markup")
-markup_main = open(os.path.join(markup_path, "html.html.mustache")).read()
+# get arguments
+data_path = arguments.data
+output_path = arguments.output
+
+# validate path existencies
+if not os.path.exists(data_path):
+    print("File not found", data_path)
+    exit(1)
+if not os.path.exists(output_path):
+    print("File not found", output_path)
+    exit(1)
+
+# get paths
+shared_path = os.path.join("src", "shared", "markup")
+template = open(os.path.join(shared_path, "html.html.mustache")).read()
 if six.PY2:
-    markup_main = unicode(markup_main)
-markup = pystache.parse(markup_main)
+    template = unicode(template)
+data = json.loads(open(os.path.join(data_path, "data.json")).read())
+renderer = pystache.Renderer(search_dirs=[data_path, shared_path], string_encoding="utf-8", file_encoding="utf-8")
 
-# get pages to create
-pages_path = os.path.join("src", "pages")
-pages = seq(os.listdir(pages_path)).map(lambda x: os.path.join(pages_path, x)).filter(os.path.isdir).list()
+# get output content
+content = renderer.render(template, data)
+if six.PY2:
+    content = content.encode("utf-8")
+else:
+    content = str(content)
 
-# generate formatted pages
-for page in pages:
-    data = json.loads(open(os.path.join(page, "data.json")).read())
-    renderer = pystache.Renderer(search_dirs=[page, markup_path], file_encoding="utf-8")
-    content = renderer.render(markup, data)
-    if six.PY2:
-        content = content.encode("utf-8")
-    else:
-        content = str(content)
-    target = open(os.path.join(output, os.path.basename(page), "index.html"), "w+")
-    target.write(content)
-    target.close()
+# get target file
+if os.path.isdir(output_path):
+    target = os.path.join(output_path, "index.html")
+elif os.path.isfile(output_path):
+    target = output_path
+else:
+    print("Output path is not directory nor file")
+    exit(1)
+
+target = open(target, "w+")
+target.write(content)
+target.close()
