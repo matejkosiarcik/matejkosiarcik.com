d477c4452fddc4c820f9e547f926f3c77d4e8ddb
Matej Košiarčik <matejkosiarcik@gmail.com>
2017-08-05 18:01:39 +0200

    [CONF] Include normalize.css into build stage

---
+++ utils/internal/build-dependencies.sh
@@ -0,0 +1,17 @@
+# shellcheck shell=sh
+#
+# This file is part of personal-website which is released under MIT license.
+# See file LICENSE.txt or go to https://github.com/matejkosiarcik/personal-website for full license details.
+#
+
+# parse arguments
+while getopts "o:" argument; do
+    case "${argument}" in o) output="${OPTARG}" ;; esac
+done
+
+if [ -z "${output}" ]; then
+    printf "%s\n" "Output path not supplied."
+    exit 1
+fi
+
+cp "./node_modules/normalize.css/normalize.css" "./${output}/_include/styles/normalize.css"
