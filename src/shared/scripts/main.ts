439c51fdbb554ff49e994fb808ee0e0ca93e136b
Matej Košiarčik <matejkosiarcik@gmail.com>
2017-08-07 13:50:38 +0200

    [ADD] Make basic script

---
+++ src/shared/scripts/main.ts
@@ -0,0 +1,6 @@
+//
+// This file is part of personal-website which is released under MIT license.
+// See file LICENSE.txt or go to https://github.com/matejkosiarcik/personal-website for full license details.
+//
+
+console.log("foo")
