dbf2b0b315c32b82822925051d672a585d3ae4e7
Matej Košiarčik <matejkosiarcik@gmail.com>
2017-08-07 14:00:44 +0200

    [ADD] Make touch device detection

---
+++ src/shared/scripts/_helpers.ts
@@ -0,0 +1,13 @@
+//
+// This file is part of personal-website which is released under MIT license.
+// See file LICENSE.txt or go to https://github.com/matejkosiarcik/personal-website for full license details.
+//
+
+export function rewrite_class(htmlclass: string, element: HTMLElement, enable: boolean): void {
+    const classes = element.classList
+    classes.remove("no-" + htmlclass)
+    classes.remove(htmlclass)
+
+    const new_class = enable ? htmlclass : "no-" + htmlclass
+    classes.add(new_class)
+}
