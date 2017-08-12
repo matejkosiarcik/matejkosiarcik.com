807c6333c7aa1ba960fd05a6da1a25fb4d3321c0
Matej Košiarčik <matej@binarytrex.com>
2017-08-12 14:43:40 +0200

    [REF] Refactor server accesing tests

--- tests/ServerTests/ServerTests.swift
+++ tests/ServerTests/ServerTests.swift
@@ -6,38 +6,40 @@
 import Alamofire
 import XCTest
 
-class RedirectionTests: XCTestCase {
+class ServerTests: XCTestCase {
     private let isLocal = true
     private let isDebug = true
     private lazy var host: String = self.isLocal ? "example.com" : "binarytrex.com"
     private lazy var redirectCode: Int = self.isDebug ? 302 : 301
+}
 
-    private var manager: Alamofire.SessionManager {
+// MARK: - Helpers
+extension ServerTests {
+    private func manager() -> Alamofire.SessionManager {
         // disable validating local server, otherwise requests fail
         let policies: [String: ServerTrustPolicy] = [
             "example.com": .disableEvaluation,
             "www.example.com": .disableEvaluation,
-        ]
+            ]
         return Alamofire.SessionManager(serverTrustPolicyManager: ServerTrustPolicyManager(policies: policies))
     }
-}
 
-// MARK: - Helpers
-extension RedirectionTests {
-    func request(url: URLConvertible) -> [Response] {
+    private func request(url: URLConvertible) -> [Response] {
         // given
         var responses: [Response] = []
-        let manager = self.manager
+        let manager = self.manager()
 
         // when
         let exp = expectation(description: "Wait for server response")
         manager.delegate.taskWillPerformHTTPRedirection = { _, _, response, request in
-            responses.append(Response(status: response.statusCode, source: response.url?.absoluteString,
+            responses.append(Response(status: response.statusCode,
+                                      source: response.url?.absoluteString,
                                       destination: request.url?.absoluteString))
             return request
         }
         manager.request(url).response {
-            responses.append(Response(status: $0.response?.statusCode, source: $0.request?.url?.absoluteString,
+            responses.append(Response(status: $0.response?.statusCode,
+                                      source: $0.request?.url?.absoluteString,
                                       destination: $0.response?.url?.absoluteString))
             exp.fulfill()
         }
@@ -46,15 +48,12 @@ extension RedirectionTests {
         wait(for: [exp], timeout: self.isLocal ? 0.2 : 1)
         return responses
     }
-}
 
-// MARK: - Access
-extension RedirectionTests {
     private func helpTestCombinations(source: String, destination: String,
                                       file: StaticString = #file, line: UInt = #line) {
         // given
         let prefices: [String] = ["http://", "https://"].flatMap { form in ["www.", ""].flatMap { form + $0 } }
-        let slashes = ["/", "//", "///", "////", "/////"]
+        let slashes = ["/", "//", "///"]
         let trailingSlashes = [""] + slashes
         let sources: [String] = prefices.flatMap { prefix in
             slashes.flatMap { slash in
@@ -74,31 +73,19 @@ extension RedirectionTests {
                            line: line)
         }
     }
+}
 
-    func testAccessRoot() {
+// MARK: - Accessing server
+extension ServerTests {
+    func testAccessPages() {
         // given
-        let source = self.host
-        let destination = "https://" + self.host + "/"
+        let resources = ["", "/about", "/zenplayer"]
+        let sources = resources.map { self.host + $0 }
+        let destinations = resources.map { "https://" + self.host + $0 + "/" }
 
         // then
-        self.helpTestCombinations(source: source, destination: destination)
-    }
-
-    func testAccessAbout() {
-        // given
-        let source = self.host + "/about"
-        let destination = "https://" + self.host + "/about/"
-
-        // then
-        self.helpTestCombinations(source: source, destination: destination)
-    }
-
-    func testAccessZenplayer() {
-        // given
-        let source = self.host + "/zenplayer"
-        let destination = "https://" + self.host + "/zenplayer/"
-
-        // then
-        self.helpTestCombinations(source: source, destination: destination)
+        zip(sources, destinations).forEach {
+            self.helpTestCombinations(source: $0.0, destination: $0.1)
+        }
     }
 }
