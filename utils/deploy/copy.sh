bf0b8c5ce07ebe8b121acf5ee685324627236eba
Matej Košiarčik <matejkosiarcik@gmail.com>
2017-12-25 21:52:44 +0100

    [FMT] Fix lint script warnings

--- utils/deploy/copy.sh
+++ utils/deploy/copy.sh
@@ -12,14 +12,14 @@ while getopts 'u:p:d:' opt; do
 done
 
 # validate arguments
-[ -z "${user}" ] && { printf "user is missing" >&2 && exit 1; }
-[ -z "${password}" ] && { printf "password is missing" >&2 && exit 1; }
-[ -z "${path}" ] && { printf "path is missing" >&2 && exit 1; }
+[ -z "${user}" ] && { printf 'user is missing\n' >&2 && exit 1; }
+[ -z "${password}" ] && { printf 'password is missing\n' >&2 && exit 1; }
+[ -z "${path}" ] && { printf 'path is missing\n' >&2 && exit 1; }
 
 # get files to upload
 commands="$(mktemp)"
-find "${path}" -type d -depth 1 -exec printf "mirror -R %s\n" "{}" \; >>"${commands}"
-find "${path}" -type f -depth 1 -exec printf "put %s\n" "{}" \; >>"${commands}"
+find "${path}" -type d -depth 1 -exec printf 'mirror -R %s\n' {} \; >>"${commands}"
+find "${path}" -type f -depth 1 -exec printf 'put %s\n' {} \; >>"${commands}"
 commands="$(cat "${commands}" && rm -f "${commands}")"
 
 # upload files
