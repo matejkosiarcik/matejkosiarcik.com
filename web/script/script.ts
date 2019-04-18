fc23e91e92820d15c1bb3d452b673796269ada95
Matej Košiarčik <matej.kosiarcik@gmail.com>
2019-04-18 19:43:34 +0200

    [WEB] Remove unneeded cast

--- web/script/script.ts
+++ web/script/script.ts
@@ -1,4 +1,4 @@
-Array.from(document.querySelectorAll<HTMLImageElement>("img")).forEach((img) => {
+Array.from(document.querySelectorAll("img")).forEach((img) => {
     img.onerror = () => {
         const sourceURL = img.getAttribute("src")
         if (sourceURL === null || sourceURL.length === 0) { return }
