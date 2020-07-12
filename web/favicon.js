7bc44767f666a8cdc51d48c0ece1992f907788bd
Matej Košiarčik <matej.kosiarcik@gmail.com>
2020-07-12 13:36:06 +0200

    build: move some favicon processing to node script

--- web/favicon.js
+++ web/favicon.js
@@ -1,11 +1,22 @@
 const svg2img = require('svg2img')
 const fs = require('fs')
 const path = require('path')
-// TODO: check favicons-webpack-plugin
+const execa = require('execa')
 
 svg2img(path.join(__dirname, 'assets', 'favicon', 'favicon.svg'),
     { width: 64, height: 64, preserveAspectRatio: true },
     (_, buffer) => {
         fs.writeFileSync(path.join(__dirname, 'assets', 'favicon', 'favicon.png'), buffer)
     }
-)
+);
+
+(async () => {
+    execa('convert', [
+        '-define', 'icon:auto-resize=32,16',
+        '-background', 'none',
+        '-colors', '256',
+        '-density', '1000',
+        path.join(__dirname, 'assets', 'favicon', 'favicon.png'),
+        path.join(__dirname, 'assets', 'favicon', 'favicon.ico')
+    ])
+})()
