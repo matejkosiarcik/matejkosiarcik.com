83cd31d531d8f887bb707056e2d160808b521b9c
Matej Košiarčik <matej.kosiarcik@gmail.com>
2020-08-02 18:58:58 +0200

    feat(analytics): restructure analytics subproject and return svg from function

--- analytics/src/index.ts
+++ analytics/src/index.ts
@@ -11,11 +11,20 @@ const cors = CorsFactory({
 
 export const analytics = functions.https.onRequest((request, response) => {
     return cors(request, response, () => {
+        // init response
         response.status(200)
-        response.end()
-        functions.logger.debug(request.headers)
-        functions.logger.debug(request.ip)
 
+        // set no caching
+        response.vary('*')
+        response.setHeader('Cache-Control', 'no-store')
+
+        // send content
+        response.type('svg')
+        response.send('<?xml version="1.0" encoding="utf-8"?><svg xmlns="http://www.w3.org/2000/svg" height="0" viewBox="0 0 0 0" width="0"></svg>')
+        response.end()
+
+        // get data for save
+        functions.logger.debug('Raw headers', request.headers)
         let data = {
             'date': new Date().toISOString(),
             'data': {
@@ -26,7 +35,9 @@ export const analytics = functions.https.onRequest((request, response) => {
                 'ip': request.ip ?? request.ips.filter(el => el)[0] ?? request.headers['x-forwarded-for'] ?? '',
             },
         }
-        functions.logger.debug(data)
+
+        // save data
+        functions.logger.debug('Saving:', data)
         database.push(data)
     })
 })
