51977890a29600c06db756f5ec35159c9d82b7fa
Matej Košiarčik <matej.kosiarcik@gmail.com>
2020-08-03 17:14:18 +0200

    style: fix js commas

--- web/.eslintrc.js
+++ web/.eslintrc.js
@@ -20,5 +20,5 @@ module.exports = {
         'eqeqeq': ['error', 'always'],
         'semi': ['error', 'never', { 'beforeStatementContinuationChars': 'never' }],
         'no-inner-declarations': 0,
-    }
+    },
 }
