d440e682e0f279ba6b960d4ce0628a2212d616f5
Matej Košiarčik <matej.kosiarcik@gmail.com>
2020-08-07 22:04:09 +0200

    style: mark comments in stylelint to check github-actions

--- web/stylelint.config.js
+++ web/stylelint.config.js
@@ -5,14 +5,14 @@ module.exports = {
         "stylelint-config-property-sort-order-smacss",
     ],
     "rules": {
-        // vendor prefices
+        // vendor prefices before
         "at-rule-no-vendor-prefix": true,
         "media-feature-name-no-vendor-prefix": true,
         "property-no-vendor-prefix": true,
         "selector-no-vendor-prefix": true,
         "value-no-vendor-prefix": true,
 
-        // quotes
+        // quotes before
         "font-family-name-quotes": "always-where-recommended",
         "function-url-quotes": "always",
         "selector-attribute-quotes": "always",
