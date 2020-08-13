fe29860f88b276dc5e66e607afdcffa673cf140b
Matej Košiarčik <matej.kosiarcik@gmail.com>
2020-08-14 01:44:36 +0200

    build: optimize svgs

--- web/gulpfile.js
+++ web/gulpfile.js
@@ -1,8 +1,15 @@
 const gulp = require('gulp')
 const sriHash = require('gulp-sri-hash')
+const svgo = require('gulp-svgo')
 
 gulp.task('sri', () => {
     return gulp.src('public/**/*.html')
         .pipe(sriHash())
+        .pipe(gulp.dest('public'))
+})
+
+gulp.task('svgo', () => {
+    return gulp.src('public/**/*.svg')
+        .pipe(svgo())
         .pipe(gulp.dest('public'))
 })
