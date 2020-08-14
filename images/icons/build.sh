a10a13c21662954fca49af63113a018fd7dc2fd4
Matej Košiarčik <matej.kosiarcik@gmail.com>
2020-08-14 09:52:45 +0200

    build: create fallback for icons

--- images/icons/build.sh
+++ images/icons/build.sh
@@ -2,7 +2,5 @@
 set -euf
 cd "$(dirname "${0}")"
 
-find . -name '{terminal,placeholder}.svg' | sed -E 's~.*/~~' | while read -r file; do
-    filename="$$(basename "$${file}" .svg)"
-    sed 's~#333~#eee~g' <"$${file}" >"generated/$${filename}-dark.svg"
-done
+mkdir -p artifacts
+node build.js
