63b3598b6d8f38a56f293e573116cc99f1c3c2b1
Matej Košiarčik <matej.kosiarcik@gmail.com>
2020-08-14 02:59:21 +0200

    refactor: simplify favicon script

--- images/favicon/build.js
+++ images/favicon/build.js
@@ -1,7 +1,6 @@
 const svg2img = require('svg2img')
 const fs = require('fs')
 const path = require('path')
-const execa = require('execa')
 
 const outDir = path.join(__dirname, 'artifacts')
 if (!fs.existsSync(outDir)) {
@@ -9,55 +8,19 @@ if (!fs.existsSync(outDir)) {
 }
 
 (async () => {
-    await new Promise((resolve, reject) => {
-        svg2img(path.join(__dirname, 'favicon.svg'),
-            { width: 16, height: 16, preserveAspectRatio: true },
-            (_, buffer) => {
-                fs.writeFileSync(path.join(outDir, '.favicon-16.png'), buffer)
-                resolve()
-            },
-        )
-        svg2img(path.join(__dirname, 'favicon.svg'),
-            { width: 32, height: 32, preserveAspectRatio: true },
-            (_, buffer) => {
-                fs.writeFileSync(path.join(outDir, '.favicon-32.png'), buffer)
-                resolve()
-            },
-        )
-        svg2img(path.join(__dirname, 'favicon.svg'),
-            { width: 48, height: 48, preserveAspectRatio: true },
-            (_, buffer) => {
-                fs.writeFileSync(path.join(outDir, '.favicon-48.png'), buffer)
-                resolve()
-            },
-        )
-        svg2img(path.join(__dirname, 'favicon.svg'),
-            { width: 64, height: 64, preserveAspectRatio: true },
-            (_, buffer) => {
-                fs.writeFileSync(path.join(outDir, '.favicon-64.png'), buffer)
-                resolve()
-            },
-        )
-        svg2img(path.join(__dirname, 'favicon.svg'),
-            { width: 96, height: 96, preserveAspectRatio: true },
-            (_, buffer) => {
-                fs.writeFileSync(path.join(outDir, '.favicon-96.png'), buffer)
-                resolve()
-            },
-        )
-        svg2img(path.join(__dirname, 'favicon.svg'),
-            { width: 128, height: 128, preserveAspectRatio: true },
-            (_, buffer) => {
-                fs.writeFileSync(path.join(outDir, '.favicon-128.png'), buffer)
-                resolve()
-            },
-        )
-        svg2img(path.join(__dirname, 'favicon.svg'),
-            { width: 256, height: 256, preserveAspectRatio: true },
-            (_, buffer) => {
-                fs.writeFileSync(path.join(outDir, '.favicon-256.png'), buffer)
-                resolve()
-            },
-        )
+    async function createSize(size) {
+        await new Promise(resolve => {
+            svg2img(path.join(__dirname, 'favicon.svg'),
+                { width: size, height: size, preserveAspectRatio: true },
+                (_, buffer) => {
+                    fs.writeFileSync(path.join(outDir, `.favicon-${size}.png`), buffer)
+                    resolve()
+                },
+            )
+        })
+    }
+
+    [16, 32, 48, 64, 96, 128, 256].forEach(async size => {
+        await createSize(size)
     })
 })()
