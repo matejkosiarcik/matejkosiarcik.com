709876be675a43ca9efe68ad8379e2da21f9a35c
Matej Košiarčik <matej.kosiarcik@gmail.com>
2020-08-17 11:03:20 +0200

    feat: redirect netlify to custom domain

--- web/script/redirect.ts
+++ web/script/redirect.ts
@@ -3,7 +3,6 @@ import { ready } from "./utils"
 // redirect from matejkosiarcik.netlify.app -> matejkosiarcik.com
 ready(() => {
     if (location.hostname.includes('netlify')) {
-        console.log('Redirect to matejkosiarcik.com/...')
-        // location.assign(`https://matejkosiarcik.com${location.pathname}`)
+        location.assign(`https://matejkosiarcik.com${location.pathname}`)
     }
 })
