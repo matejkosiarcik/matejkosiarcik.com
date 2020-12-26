df20dc13ff467685ed7975b91364c7d246999630
Matej Košiarčik <matej.kosiarcik@gmail.com>
2020-12-26 10:09:27 +0100

    build: remove docker icon copying

--- web/webpack.config.js
+++ web/webpack.config.js
@@ -34,7 +34,6 @@ const plugins = glob.sync(`${htmlDir}/**/*.html`, { nodir: true }).map(file => n
         patterns:
             [].concat(
                 glob.sync('../images/favicon/{,artifacts/}*.{svg,png,ico}').map(file => { return { from: file, to: '' } }),
-                glob.sync('../images/docker/artifacts/*.{jpg,png,svg,bmp,gif,webp}').map(file => { return { from: file, to: 'img' } }),
                 glob.sync('../images/icons/{original,artifacts}/*.{jpg,png,svg,bmp,gif,webp}').map(file => { return { from: file, to: 'img' } }),
                 [
                     { from: path.join(__dirname, 'jekyll', '_site', 'sitemap.xml'), to: '' },
