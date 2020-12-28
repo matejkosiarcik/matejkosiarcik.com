31e0bc6bd3466409ec5f396399c186ae5573582d
Matej Košiarčik <matej.kosiarcik@gmail.com>
2020-12-28 10:41:05 +0100

    fix: minify goat counter properly

--- web/fetch-goat.js
+++ web/fetch-goat.js
@@ -1,5 +1,6 @@
 const fs = require('fs')
 const path = require('path')
+const execa = require('execa')
 require('isomorphic-fetch');
 
 (async () => {
@@ -8,13 +9,7 @@ require('isomorphic-fetch');
     try {
         fs.mkdirSync(path.resolve(__dirname, 'script', 'tmp'))
     } catch(e) {}
-    // TODO: replace this monstrosity with terser
-    const minifiedScript = originalScript.split('\n')
-        .map(line => line.replace(/^\s+/, '').replace(/\/\/.*$/g, ''))
-        .filter(line => line !== '')
-        .join('\n')
-        .replace(/[ \t ]+/g, ' ')
-        .replace(/, /g, ',')
-        .replace(/ ?([\+-=\|?:!&\*\(\)\[\]\{\}]+) ?/g, '$1')
-    fs.writeFileSync(path.join(__dirname, 'script', 'tmp', 'goat.js'), minifiedScript)
+    const scriptPath = path.join(__dirname, 'script', 'tmp', 'goat.js')
+    fs.writeFileSync(scriptPath, originalScript)
+    await execa('terser', ['--ie8', '--safari10', '--output', scriptPath, scriptPath])
 })()
