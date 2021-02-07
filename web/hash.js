c8e49409490de0198b8bf8bd69b17efb6e25af75
Matej Košiarčik <matej.kosiarcik@gmail.com>
2021-02-07 11:55:35 +0100

    build: refactor and comment hash.js build script

--- web/hash.js
+++ web/hash.js
@@ -3,44 +3,54 @@ const path = require('path')
 const glob = require('glob')
 const hasha = require('hasha')
 
-const resourceFiles = glob.sync('public/**/*.{css,js}');
-const htmlFiles = glob.sync('public/**/*.html');
+// glob resource files
+const assetFiles = glob.sync('public/**/*.{css,js}');
+const documentFiles = glob.sync('public/**/*.html');
 
+// returns hash of file as string
 async function fileHash(filePath) {
-    const hashBase16 = await hasha.fromFile(filePath, {algorithm: 'sha256'})
-    const hashBase36 = BigInt(`0x${hashBase16}`).toString(36)
-    return hashBase36.substr(0,10)
+    const hashBase16 = await hasha.fromFile(filePath, { algorithm: 'sha256' })
+    const hashBase36 = BigInt(`0x${hashBase16}`).toString(36) // base36 represents numbers [0-9] and letters [a-z]
+    return hashBase36.substr(0,12)
+}
+
+// replace all occurences of "pattern" in "content" with "value"
+function replaceAll(content, pattern, value) {
+    let output = content
+    while (true) {
+        let newContent = output.replace(pattern, value)
+        if (newContent === output) {
+            break
+        }
+        output = newContent
+    }
+    return output
 }
 
 (async () => {
-    const resourceHashes = await Promise.all(resourceFiles.map(fileHash))
+    const assetHashes = await Promise.all(assetFiles.map(fileHash))
+    const oldAssetNames = []
+    const newAssetNames = []
 
-    const oldFileNames = []
-    const newFileNames = []
+    // get list of asset files to rename
+    for (let i in assetFiles) {
+        const oldAssetPath = assetFiles[i]
+        const newAssetPath = assetFiles[i].replace(/\.([a-zA-Z0-9]+)$/, `.${assetHashes[i]}.$1`)
 
-    for (let i in resourceFiles) {
-        const oldPath = resourceFiles[i]
-        const newPath = resourceFiles[i].replace(/\.([a-zA-Z0-9]+)$/, `.${resourceHashes[i]}.$1`)
+        oldAssetNames.push(path.basename(oldAssetPath))
+        newAssetNames.push(path.basename(newAssetPath))
 
-        oldFileNames.push(path.basename(oldPath))
-        newFileNames.push(path.basename(newPath))
-
-        console.log(`Move ${oldPath} -> ${newPath}`)
-        fs.renameSync(oldPath, newPath)
+        console.log(`Move ${oldAssetPath} -> ${newAssetPath}`)
+        fs.renameSync(oldAssetPath, newAssetPath)
     }
 
-    htmlFiles.forEach(filePath => {
-        let fileContent = fs.readFileSync(filePath).toString('utf-8')
-        for (let i in resourceFiles) {
-            while (true) {
-                let newFileContent = fileContent.replace(oldFileNames[i], newFileNames[i])
-                if (newFileContent === fileContent) {
-                    break
-                }
-                fileContent = newFileContent
-            }
+    // rename asset files in documents
+    documentFiles.forEach(documentFilePath => {
+        let documentContent = fs.readFileSync(documentFilePath).toString('utf-8')
+        for (let i in assetFiles) {
+            documentContent = replaceAll(documentContent, oldAssetNames[i], newAssetNames[i])
         }
-
-        fs.writeFileSync(filePath, fileContent, { encoding: 'utf-8' })
+        fs.writeFileSync(documentFilePath, documentContent, { encoding: 'utf-8' })
     })
+    // TODO: also rename assets in other assets
 })()
