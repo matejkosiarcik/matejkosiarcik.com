3d644e7c3e01753da4257e3fd35297687e52adf5
Matej Košiarčik <matej.kosiarcik@gmail.com>
2021-03-27 10:58:17 +0100

    build: fix images/icons build script

--- images/icons/build.js
+++ images/icons/build.js
@@ -2,22 +2,20 @@ const fs = require('fs');
 const path = require('path');
 const glob = require('glob-all');
 const execa = require('execa');
-
-const outDir = path.join(__dirname, 'artifacts');
-try {
-  fs.mkdirSync(outDir);
-} catch (e) { return; }
-if (!fs.existsSync(outDir)) {
-  console.log('Could not create/find output directory');
-}
-
-process.chdir(__dirname);
+const process = require('process');
 
 async function svg2png(inputFile, outputFile, width, height) {
   await execa('rsvg-convert', ['-f', 'png', '-w', `${width}`, '-h', `${height}`, inputFile, '-o', outputFile]);
 }
 
 (async () => {
+  process.chdir(__dirname);
+
+  const outDir = path.join(__dirname, 'artifacts');
+  if (!fs.existsSync(outDir)) {
+    console.log("Can't find output directory");
+  }
+
   await Promise.all(
     glob.sync(['original/{placeholder,terminal,docker,warning}.svg'])
       .map((inputFile) => svg2png(inputFile, path.join(outDir, `${path.basename(inputFile, '.svg')}.png`), 80, 80)),
