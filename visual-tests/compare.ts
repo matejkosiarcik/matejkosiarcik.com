617c4df4b9cc9d1c1874d8cc3eda9881bfffbb93
Matej Košiarčik <matej.kosiarcik@gmail.com>
2021-07-01 23:54:01 +0200

    test: extract main functions into library for visual-tests

--- visual-tests/compare.ts
+++ visual-tests/compare.ts
@@ -1,7 +1,6 @@
 import * as fs from 'fs/promises';
 import * as path from 'path';
-import { PNG } from 'pngjs';
-import * as pixelmatch from 'pixelmatch';
+import { compare, report } from './library';
 
 const treshold = 0.001;
 
@@ -49,9 +48,7 @@ function union(arr1: string[], arr2: string[]): string[] {
   return resultArray;
 }
 
-// TODO: allow comparing images with nonmatching sizes
-// create diff data
-async function createDiffs() : Promise<{ name: string, diff: number }[]> {
+(async () => {
   const [testFiles, referenceFiles] = await Promise.all([
     Promise.all((await listDirRecursive('test')).map((file) => file.replace(/^test./, ''))),
     Promise.all((await listDirRecursive('reference')).map((file) => file.replace(/^reference./, ''))),
@@ -59,93 +56,10 @@ async function createDiffs() : Promise<{ name: string, diff: number }[]> {
   ]);
   const allFiles = union(testFiles, referenceFiles);
 
-  const imgDiffs: { name: string, diff: number }[] = [];
-  await Promise.all(allFiles.map(async (file) => {
-    const testFile = path.join('test', file);
-    const referenceFile = path.join('reference', file);
-    const diffFile = path.join('diff', file);
-    const [testStats, referenceStats] = await Promise.all([
-      fs.stat(testFile),
-      fs.stat(referenceFile),
-      fs.mkdir(path.dirname(diffFile), { recursive: true }),
-    ]);
-    if (!testStats.isFile()) {
-      console.error(`Test file ${testFile} does not exist`);
-      return;
-    }
-    if (!referenceStats.isFile()) {
-      console.error(`Reference file ${referenceFile} does not exist`);
-      return;
-    }
+  const comparisons = await Promise.all(allFiles.map((img) => compare(img, 'reference', 'test', 'diff')));
+  const success = await report(comparisons, { treshold: treshold });
 
-    // const testImg = PNG.sync.read(await fs.readFile(testFile));
-    // const referenceImg = PNG.sync.read(await fs.readFile(referenceFile));
-
-    const [testBuffer, referenceBuffer] = await Promise.all([
-      fs.readFile(testFile),
-      fs.readFile(referenceFile),
-    ]);
-    const [testImg, referenceImg]: PNG[] = await Promise.all([
-      new Promise((resolve) => {
-        const img = new PNG();
-        img.parse(testBuffer, () => resolve(img));
-      }),
-      new Promise((resolve) => {
-        const img = new PNG();
-        img.parse(referenceBuffer, () => resolve(img));
-      }),
-    ]);
-
-    const diffImg = new PNG({ width: Math.max(testImg.width, referenceImg.width), height: Math.max(testImg.height, referenceImg.height) });
-    const totalPixelsCount = diffImg.height * diffImg.width;
-    try {
-      const diffPixelsCount = pixelmatch(testImg.data, referenceImg.data, diffImg.data, diffImg.width, diffImg.height, { threshold: 0 });
-      imgDiffs.push({ name: diffFile, diff: diffPixelsCount / totalPixelsCount });
-    } catch (error) {
-      imgDiffs.push({ name: diffFile, diff: NaN });
-    }
-    await fs.writeFile(diffFile, PNG.sync.write(diffImg));
-  }));
-
-  return imgDiffs;
-}
-
-// report diffs to CLI
-async function report(imgDiffs: { name: string, diff: number }[]): Promise<boolean> {
-  imgDiffs.sort((a, b) => a.name.localeCompare(b.name, undefined, { numeric: true, ignorePunctuation: true }));
-
-  const maxImgNameLength = Math.max(...imgDiffs.map((el) => el.name.length));
-  const maxDiffLength = '100.00%'.length;
-
-  console.log(`Treshold: ${(treshold * 100).toFixed(3)}`);
-  console.log(`| ${'-'.repeat(maxImgNameLength)} | ${'-'.repeat(maxDiffLength)} |`);
-  console.log(`| image${' '.repeat(maxImgNameLength - 5)} | ${' '.repeat(maxDiffLength - 4)}diff |`);
-  console.log(`| ${'-'.repeat(maxImgNameLength)} | ${'-'.repeat(maxDiffLength)} |`);
-  let failsCount = 0;
-  for (const img of imgDiffs) {
-    const isSuccess = img.diff <= treshold;
-    failsCount += isSuccess ? 0 : 1;
-
-    const colorStart = isSuccess ? (img.diff > 0 ? '\u001b[33m' : '\u001b[32m') : '\u001b[31m'; // eslint-disable-line no-nested-ternary
-    const colorEnd = '\u001b[0m';
-
-    const diff = `${Number.isFinite(img.diff) ? (img.diff * 100).toFixed(2) : '-'}%`;
-    const nameSpacer = ' '.repeat(maxImgNameLength - img.name.length);
-    const diffSpacer = ' '.repeat(maxDiffLength - diff.length);
-    const trailer = isSuccess ? '' : ' ❌';
-    console.log(`| ${img.name}${nameSpacer} | ${diffSpacer}${colorStart}${diff}${colorEnd} |${trailer}`);
-  }
-  console.log(`| ${'-'.repeat(maxImgNameLength)} | ${'-'.repeat(maxDiffLength)} |`);
-  console.log(`Found ${failsCount} failing images`);
-
-  return failsCount === 0;
-}
-
-(async () => {
-  const imgDiffs = await createDiffs();
-  const isGlobalSuccess = await report(imgDiffs);
-
-  if (!isGlobalSuccess) {
+  if (!success) {
     process.exit(1);
   }
 })();
