c1d60bf410634a8ac6cdebdc538ccee6cd934b64
Matej Košiarčik <matej.kosiarcik@gmail.com>
2021-07-17 19:02:26 +0200

    test: update visual-tests to have single main.ts file nad library.ts (prepare for extracting this to external package)

--- visual-tests/main.ts
+++ visual-tests/main.ts
@@ -1,12 +1,15 @@
 import * as playwright from 'playwright';
 import * as fs from 'fs/promises';
 import * as path from 'path';
+import { compare, report, VizzResult } from './library';
 
 const baseUrl = process.env.BASE_URL;
 if (!baseUrl) {
   console.error('BASE_URL not set.');
   process.exit(1);
 }
+
+const treshold = 0.001;
 
 const pages = [
   {
@@ -63,14 +66,15 @@ const browsers = [
 
 (async () => {
   await fs.rm('test', { force: true, recursive: true });
+  const comparisons: VizzResult[] = [];
 
   await Promise.all(browsers.map(async (browserInfo) => {
     const browser = await browserInfo.browser.launch({ headless: true });
     const context = await browser.newContext();
 
     await Promise.all(pages.map(async (pageInfo) => {
-      const viewportDir = path.join('test', pageInfo.name, browserInfo.name, 'viewport');
-      const fullpageDir = path.join('test', pageInfo.name, browserInfo.name, 'full');
+      const viewportDir = path.join(pageInfo.name, browserInfo.name, 'viewport');
+      const fullpageDir = path.join(pageInfo.name, browserInfo.name, 'full');
       const [page] = await Promise.all([
         context.newPage(),
         fs.mkdir(viewportDir, { recursive: true }),
@@ -82,12 +86,20 @@ const browsers = [
       for (const viewport of viewports) {
         await page.setViewportSize(viewport);
         await page.goto(`${baseUrl}${pageInfo.url}`, { waitUntil: 'networkidle' });
-        // load all async resources as sync
+
+        // force load all async resources
         await page.$$eval('[loading=lazy]', (elements) => elements.forEach((element) => element.setAttribute('loading', 'eager')));
+
+        const viewportImg = path.join(viewportDir, `${viewport.width}x${viewport.height}.png`);
+        const fullpageImg = path.join(fullpageDir, `${viewport.width}x${viewport.height}.png`);
         await Promise.all([
-          page.screenshot({ path: path.join(viewportDir, `${viewport.width}x${viewport.height}.png`) }),
-          page.screenshot({ path: path.join(fullpageDir, `${viewport.width}x${viewport.height}.png`), fullPage: true }),
+          page.screenshot({ path: path.join('test', viewportImg) }),
+          page.screenshot({ path: path.join('test', fullpageImg), fullPage: true }),
         ]);
+        comparisons.push(...await Promise.all([
+          compare(viewportImg, 'reference', 'test', 'diff'),
+          compare(fullpageImg, 'reference', 'test', 'diff'),
+        ]));
       }
 
       await page.close();
@@ -95,4 +107,9 @@ const browsers = [
 
     await browser.close();
   }));
+
+  const success = await report(comparisons, { treshold: treshold });
+  if (!success) {
+    process.exit(1);
+  }
 })();
