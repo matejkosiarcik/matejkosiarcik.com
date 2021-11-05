1b0e8c431e2ef49ec0696a9ebd1721b45fff0ed3
Matej Košiarčik <matej.kosiarcik@gmail.com>
2021-11-05 12:49:20 +0100

    test(visual): use dotenv to manage environments

--- visual-tests/jest.config.js
+++ visual-tests/jest.config.js
@@ -1,3 +1,10 @@
+const process = require('process');
+const dotenv = require('dotenv');
+
+if (process.env.NODE_ENV) {
+  config = dotenv.config({ path: `${process.cwd()}/${process.env.NODE_ENV}.env` });
+}
+
 module.exports = {
   preset: 'jest-playwright-preset',
   transform: {
