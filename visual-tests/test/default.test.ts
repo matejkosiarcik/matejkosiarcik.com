d8844a763de4405848b44d28209aa6c6d12e8003
Matej Košiarčik <matej.kosiarcik@gmail.com>
2021-11-16 11:37:54 +0100

    test: update visual-tests

--- visual-tests/test/default.test.ts
+++ visual-tests/test/default.test.ts
@@ -1,31 +1,31 @@
 import { configureToMatchImageSnapshot } from 'jest-image-snapshot';
-import viewports from '../utils/viewports';
-import locations from '../utils/locations';
+import { locations, viewports } from '../utils/constants';
 
-const toMatchImageSnapshot = configureToMatchImageSnapshot({
-  failureThreshold: 0.05,
-  failureThresholdType: 'percent',
-  customSnapshotsDir: 'snapshots',
-  updatePassedSnapshot: true,
+expect.extend({
+  toMatchImageSnapshot: configureToMatchImageSnapshot({
+    failureThreshold: 0.05,
+    failureThresholdType: 'percent',
+    customSnapshotsDir: 'snapshots',
+    updatePassedSnapshot: true,
+  })
 });
-expect.extend({ toMatchImageSnapshot });
 
 describe.each(locations)('$name', (location: typeof locations[0]) => {
   beforeAll(async () => {
     await page.goto(`${baseUrl}${location.url}`, { waitUntil: 'load' });
 
     // Force load all images
-    await page.$$eval('[loading=lazy]', (elements) => elements.forEach((element) => element.setAttribute('loading', 'eager')));
+    await page.$$eval('[loading],img,iframe', (elements) => {
+      elements.forEach((element) => {
+        element.setAttribute('loading', 'eager');
+      });
+    });
   });
 
   test.each(viewports)('$width x $height', async (viewport: typeof viewports[0]) => {
     await page.setViewportSize({ width: viewport.width, height: viewport.height });
 
-    const [landingImage, fullpageImage] = await Promise.all([
-      page.screenshot({ fullPage: false }),
-      page.screenshot({ fullPage: true }),
-    ]);
-    expect(landingImage).toMatchImageSnapshot({ customSnapshotIdentifier: `${location.name}_${viewport.width}x${viewport.height}_landing_${browserName}` });
-    expect(fullpageImage).toMatchImageSnapshot({ customSnapshotIdentifier: `${location.name}_${viewport.width}x${viewport.height}_fullpage_${browserName}` });
+    const fullpageImage = await page.screenshot({ fullPage: true });
+    expect(fullpageImage).toMatchImageSnapshot({ customSnapshotIdentifier: `${location.name}_${viewport.width}x${viewport.height}_${browserName}` });
   });
 });
---
+++ visual-tests/utils/constants.ts
@@ -0,0 +1,54 @@
+export const locations = [
+  {
+    name: 'home',
+    url: '/',
+  },
+  {
+    name: 'projects',
+    url: '/projects',
+  },
+  {
+    name: 'about',
+    url: '/about',
+  },
+  {
+    name: 'blog',
+    url: '/blog',
+  },
+  {
+    name: '404',
+    url: '/404',
+  },
+  {
+    name: 'zenplayer',
+    url: '/zenplayer',
+  },
+];
+
+export const viewports = [
+  {
+    label: 'Phone',
+    width: 375,
+    height: 667,
+  },
+  {
+    label: 'Tablet',
+    width: 1024,
+    height: 768,
+  },
+  {
+    label: 'HD',
+    width: 1280,
+    height: 720,
+  },
+  {
+    label: 'FullHD',
+    width: 1920,
+    height: 1080,
+  },
+  {
+    label: 'UltraHD',
+    width: 3840,
+    height: 2160,
+  },
+];
